8d470591556f258000eb27e19b5d43f16a48e9f7
JamesL <jlawler@gmail.com>
2012-10-18 23:16:23 +0100

    Add clear function Enabled saving to local storage

--- www/app/js/controllers.js
+++ www/app/js/controllers.js
@@ -4,13 +4,21 @@
 
 function HowOftenCtrl($scope) {
     $scope.newTaskDescription = '';
-    $scope.tasks = new Array();
+    $scope.taskRepository = new TasksRepository();
+    $scope.tasks = $scope.taskRepository.GetTasks();
 
     $scope.addTask = function() {
          $scope.tasks.push(new Task($scope.newTaskDescription));
+         $scope.taskRepository.SaveTasks($scope.tasks);
+    };
+
+    $scope.clearTasks = function() {
+          $scope.tasks = new Array();
+          $scope.tasksRepository.SaveTasks($scope.tasks);
     };
 
     $scope.markTask = function(task) {
         task.Occurrences.push(new Date())
+        $scope.taskRepository.SaveTasks($scope.tasks);
     };
 }
