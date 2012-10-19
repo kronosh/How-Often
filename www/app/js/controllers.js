bc7a6e505c49b6e4707f11e2dac638b3e7f2db0a
JamesL <jlawler@gmail.com>
2012-10-19 08:06:31 +0100

    Test debug

--- www/app/js/controllers.js
+++ www/app/js/controllers.js
@@ -2,23 +2,44 @@
 
 /* Controllers */
 
-function HowOftenCtrl($scope) {
+function HowOftenCtrl($scope, $log) {
+    $log.warn('Started controller');
+
     $scope.newTaskDescription = '';
-    //$scope.taskRepository = new TasksRepository();
+    $scope.taskRepository = new TasksRepository();
     //$scope.tasks = $scope.taskRepository.GetTasks();
     $scope.tasks = new Array();
 
+    $log.warn('Directly saving to local storage');
+
+    window.localStorage['test'] = 'james';
+    $log.warn(window.localStorage['test']);
+
+    $log.warn('Using DAL to save to local storage')
+
+    $scope.DataAccessLayer = new LocalStorage();
+    $scope.DataAccessLayer.Save("test")
+    $log.warn($scope.DataAccessLayer.Load())
+
+    $log.warn('end of tests');
+
     $scope.addTask = function() {
-         $scope.tasks.push(new Task($scope.newTaskDescription));
-         //$scope.taskRepository.SaveTasks($scope.tasks);
+        $log.warn('Started addTask');
+
+        $scope.tasks.push(new Task($scope.newTaskDescription));
+        $scope.taskRepository.SaveTasks($scope.tasks);
     };
 
     $scope.clearTasks = function() {
-          $scope.tasks = new Array();
+        $log.warn('Started clearTasks');
+
+        $scope.tasks = new Array();
           //$scope.tasksRepository.SaveTasks($scope.tasks);
     };
 
     $scope.markTask = function(task) {
+        $log.warn('Started markTask');
+
         task.Occurrences.push(new Date())
         //$scope.taskRepository.SaveTasks($scope.tasks);
     };
