aa3f1ba8abb6f9429f4810f846411ae76491dd01
JamesL <jlawler@gmail.com>
2012-10-19 07:54:16 +0100

    Test debug

--- www/app/js/controllers.js
+++ www/app/js/controllers.js
@@ -4,21 +4,22 @@
 
 function HowOftenCtrl($scope) {
     $scope.newTaskDescription = '';
-    $scope.taskRepository = new TasksRepository();
-    $scope.tasks = $scope.taskRepository.GetTasks();
+    //$scope.taskRepository = new TasksRepository();
+    //$scope.tasks = $scope.taskRepository.GetTasks();
+    $scope.tasks = new Array();
 
     $scope.addTask = function() {
          $scope.tasks.push(new Task($scope.newTaskDescription));
-         $scope.taskRepository.SaveTasks($scope.tasks);
+         //$scope.taskRepository.SaveTasks($scope.tasks);
     };
 
     $scope.clearTasks = function() {
           $scope.tasks = new Array();
-          $scope.tasksRepository.SaveTasks($scope.tasks);
+          //$scope.tasksRepository.SaveTasks($scope.tasks);
     };
 
     $scope.markTask = function(task) {
         task.Occurrences.push(new Date())
-        $scope.taskRepository.SaveTasks($scope.tasks);
+        //$scope.taskRepository.SaveTasks($scope.tasks);
     };
 }
